5d6a6219a53750ed11d7b4e3e810a47e30e9128d
shamsKhokhar56 <shamsudinkhokhar56@gmail.com>
2020-11-27 17:54:38 +0500

    Task2: Adding text inputs for original price and discount price

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, TextInput, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [value, onChangeText] = React.useState('0');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textHeadings}>
+        Enter Original Price
+      </Text>
+      <TextInput
+        style={styles.fields}
+        onChangeText={text => onChangeText(text)}
+        value={value}
+      />
+      <Text style={styles.textHeadings}>
+        Enter Disount Percentage
+      </Text>
+      <TextInput
+        style={styles.fields}
+        onChangeText={text => onChangeText(text)}
+        value={value}
+      />
+      <StatusBar style='auto'/>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: 'black', 
+    flex: 1,
     justifyContent: 'center',
   },
-});
+  textHeadings: {
+    color: 'white',
+    fontSize: 20,
+    margin: 10
+  },
+  fields: {
+    alignItems: 'flex-end',
+    borderColor: 'green', 
+    borderWidth: 1,
+    color: 'yellow',
+    backgroundColor: 'blue',
+    textAlign: 'right',
+    height: 40,
+    justifyContent: 'flex-end',
+    // width: Dimensions.get('screen').width - 30,
+    width: 250,
+  },
+})
+
+export default App;
